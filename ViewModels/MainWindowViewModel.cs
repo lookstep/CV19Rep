0b1dee10e0204e4736d2bea32888048d40ae9d74
Nastya Nastya <Lookstep@bk.ru>
2021-09-10 00:31:10 +0300

    Тестирование поведения работы окна

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _title = "Анализ статистики CV19";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+            //set
+            //{
+            //    if (Equals(_title, value)) return;
+            //    _title = value;
+            //    OnPropertChanged();
+            //    Set(ref _title, value);
+            //}
+        }
+        #endregion
     }
 }
