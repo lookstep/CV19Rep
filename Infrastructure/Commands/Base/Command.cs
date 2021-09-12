ac1e53f8e70d55f413d0161b1bd318ca10731744
Nastya Nastya <Lookstep@bk.ru>
2021-09-12 16:58:55 +0300

    Базовый класс команды.

--- Infrastructure/Commands/Base/Command.cs
+++ Infrastructure/Commands/Base/Command.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CV19.Infrastructure.Commands.Base
 {
-    abstract internal class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
